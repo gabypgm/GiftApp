c88f73f3cac2120474ef8507467051fd540bb6f5
Gabriela Guaman <gabrielaguaman@Gabrielas-MacBook-Pro.local>
2023-02-23 14:06:04 -0500

    feat: added local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SearchGIFResponse, Gift } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,19 @@ import { HttpClient } from '@angular/common/http';
 export class GifsService {
 
   private _historial: string[] = []
-  public resultados: any[] = []
+  public resultados: Gift[] = []
   token: string = ''
 
   get historial() {
     return [...this._historial]
   }
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) {
+      localStorage.getItem('historial')
+      if(localStorage.getItem('historial')){
+        this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+      }
+   }
 
 
 
@@ -24,11 +30,12 @@ export class GifsService {
     if(!this._historial.includes(query)){
       this._historial.unshift(query)
     }
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.token}=${query}&limit=10`)
-        .subscribe((response: any) => {
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.token}=${query}&limit=10`)
+        .subscribe((response) => {
           console.log(response.data)
           this.resultados = response.data
         })
+        localStorage.setItem('historial', JSON.stringify(this._historial))
     
     console.log(this._historial)
   }
